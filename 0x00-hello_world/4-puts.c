56e08a10cc4c8173c76a196c085e3564ea98368b
Chopper-82 <maxwellowusubanahene@gmail.com>
2023-07-13 12:42:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return (O)
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }

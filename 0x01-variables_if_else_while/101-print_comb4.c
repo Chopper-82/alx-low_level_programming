19fe72f90ccdc1a8d47c33991dce9f0a29ecf32f
Chopper-82 <maxwellowusubanahene@gmail.com>
2023-07-16 10:55:07 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Description: This function prints a message using the puts function.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int a = 0;
+int b = 1;
+int c = 2;
+
+while (a <= 7)
+{
+while (b <= 8)
+{
+while (c <= 9)
+{
+if (a != b && b != c && a != c)
+{
+putchar(a + '0');
+putchar(b + '0');
+putchar(c + '0');
+
+if (!(a == 7 && b == 8 && c == 9))
+{
+putchar(',');
+putchar(' ');
+}
+}
+c++;
+}
+b++;
+c = b + 1;
+}
+a++;
+b = a + 1;
+c = b + 1;
+}
+
+putchar('\n');
+
+return (0);
+}
+

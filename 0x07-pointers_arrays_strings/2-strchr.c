5c66487382c5986b458fe8a4eb732101de959e71
Chopper-82 <maxwellowusubanahene@gmail.com>
2023-07-31 11:25:53 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * @c: character to be located
+ * @s: string to locate character in
+ * Return: Always 0
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	
+	i = 0;
+	if (s[i] = 'c')
+		return (s);
+	else
+		return (NULL)
+}

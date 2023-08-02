cfde3476fbd00447fc6b55b60aa26fb6affc99d9
Chopper-82 <maxwellowusubanahene@gmail.com>
2023-08-02 12:24:21 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - function like puts(s);
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_putchar('\n');
+	}
+}

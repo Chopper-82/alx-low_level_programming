882ae4609db51cdc7b0f4db83c00d3e1e2eb93dc
Chopper-82 <maxwellowusubanahene@gmail.com>
2023-08-04 03:02:51 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,35 @@
+#include "main."
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: Always 0 (Success)
+ */
+int main(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	printf("%d", product)
+}
+return 0;
+}#include "main."
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: Always 0 (Success)
+ */
+int main(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	printf("%d", product)
+}
+return 0;
+}
